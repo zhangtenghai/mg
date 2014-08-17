62b7f24747739278badc03dfa2b25735b1a57bec
bruce <zth5906@gmail.com>
2014-08-17 16:33:39 +0800

    config

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -10,4 +10,5 @@ CKEDITOR.editorConfig = function(config) {
   config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
   config.filebrowserImageUploadUrl = "/ckeditor/pictures";
   config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+  
 }
