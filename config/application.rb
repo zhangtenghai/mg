f75696b7df359ef60aed069dd12bbb106b18b1c4
bruce <zth5906@gmail.com>
2014-08-18 18:14:07 +0800

    assets

--- config/application.rb
+++ config/application.rb
@@ -29,20 +29,6 @@ module Mg
     config.middleware.use Rack::ContentLength
     
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
-        full_path = Rails.application.assets.resolve(path).to_path
-        app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
-          puts "including asset: " + full_path
-          true
-        else
-          puts "excluding asset: " + full_path
-          false
-        end
-      else
-        false
-      end
-    end
+    
   end
 end
