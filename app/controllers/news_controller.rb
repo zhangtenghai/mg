66a89e355dd818b858964f588a246a2b49fb6a14
bruce <zth5906@gmail.com>
2014-11-06 21:15:20 +0800

    fix bug

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,7 +4,7 @@ class NewsController < ApplicationController
     @head_news = News.get_head_news
     @games = Game.get_current_year_not_sale_games.limit(7)
     #@news = News.get_date_news(params[:date])
-    @news = News.page(params[:page]).per(20)
+    @news = News.where("id <> ?",@head_news.id).page(params[:page]).per(20)
     #@dates = @news.select("created_at").group(:created_at).distinct
     @hot_game = Game.get_today_hot_game
     @voices = Voice.limit(3)
