e0a05eb5cc03adb3754c5f1aa16fa9e8626f17c6
bruce <zth5906@gmail.com>
2015-04-15 17:30:19 +0800

    register

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,36 @@
 class RegistrationsController < Devise::RegistrationsController
- 
+
   def create
     build_resource(sign_up_params)
   
     resource_saved = resource.save
     yield resource if block_given?
-
+    p params
     if resource_saved
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
-        return render :json => {:success => true}
+        respond_to do |format|
+          format.html { respond_with resource, location: after_sign_up_path_for(resource) }
+          format.json { render :json => {:success => true} } 
+        end
+        #render :json => {:success => true}
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         expire_data_after_sign_in!
-        return render :json => {:success => true}
+        respond_to do |format|
+          format.html { respond_with resource, location: after_inactive_sign_up_path_for(resource) }
+          format.json { render :json => {:success => true} } 
+        end
+        #render :json => {:success => true}
       end
     else
-      p  resource.errors
       clean_up_passwords resource
-      return render :json => {:success => false,:msg => resource.errors}
+      respond_to do |format|
+        format.html { respond_with resource }
+        format.json { render :json => {:success => false, :msg => resource.errors} } 
+      end
+      #render :json => {:success => false,:msg => resource.errors}
     end
   end
  
@@ -31,6 +42,6 @@ class RegistrationsController < Devise::RegistrationsController
  
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :name)
+    params.require(:user).permit(:email, :password, :name, :password_confirmation)
   end
 end
